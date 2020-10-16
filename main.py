bfea3a53090d60752129869abacbccfc8ec88670
Gustavo Viera López <gvieralopez@gmail.com>
2020-10-16 13:32:54 +0200

    Add some missing comments

--- main.py
+++ main.py
@@ -4,14 +4,23 @@ import numpy as np
 from tools import frame_diff
 from settings import data_folder, data_file
 
+# Initialize Video Source
 cap = cv2.VideoCapture(os.path.join(os.getcwd(), data_folder, data_file))
-previous_frame = None
+
+# Temporal variables
+previous_frame = None # Stores last processed frame
+
+# Loop for all frames in the video
 while True:
+    # Get current frame
     ret, frame = cap.read()    
     if ret:
+        # Checks if there is a real previous frame
         if not isinstance(previous_frame, np.ndarray):
             previous_frame = frame.copy()
             continue
+
+        # Locates the region with higher pixel-change
         fd = frame_diff(previous_frame, frame)
         if isinstance(fd, np.ndarray):            
             cv2.imshow('a', fd)
@@ -19,7 +28,10 @@ while True:
         else:
             break
 
-        previous_frame = frame.copy()      
+        # Saves the current frame as 'previous' for next iteration
+        previous_frame = frame.copy()   
+        
+    # Ends the processing when no more frames detected   
     else:
         print("[INFO] No more frames to process.")
         break
