ab344a461dbe4095a16e1ae7e5dd6d32f0db7ad4
Jorge Morgado <jorge.morgadov@gmail.com>
2021-02-26 21:42:08 -0400

    basic codestyle fixes

--- tracking/trackers.py
+++ tracking/trackers.py
@@ -4,8 +4,9 @@ import numpy as np
 from tracking.algorithms import resize_frame
 from tracking.affine_estimator import get_affine
 
+
 class ROI():
-    """docstring for ROI"""
+
     def __init__(self, roi_size, init_method, scale=0.5):
         self.roi_width, self.roi_heigh = roi_size
         self.prev_cXY = None, None
@@ -37,8 +38,7 @@ class ROI():
         self.cXY = int(self.global_width/2), int(self.global_heigh/2)
         return self.cXY
 
-    def manual_init(self, frame, name,
-                       win2_name='ROI'):
+    def manual_init(self, frame, name, win2_name='ROI'):
 
         win1_name = 'Clic on the center of {} to init roi'.format(name)
 
@@ -59,11 +59,11 @@ class ROI():
                 img_ = frame_.copy()
 
                 # draw a circle in the selected pixel
-                cv2.circle(img_, (x,y), 3, (0,255,255), 1)
+                cv2.circle(img_, (x, y), 3, (0, 255, 255), 1)
                 cv2.imshow(win1_name, img_)
-                
+
                 # get roi in the full size frame
-                cv2.circle(img, self.cXY, 3, (0,255,255), 1)
+                cv2.circle(img, self.cXY, 3, (0, 255, 255), 1)
                 roi = self.crop(img)
 
                 # roi padding just to display the new window
@@ -72,7 +72,7 @@ class ROI():
                 cv2.imshow(win2_name, roi_)
 
                 print('ROI Initialized, now press any key to continue')
-        
+
         cv2.setMouseCallback(win1_name, on_click)
         cv2.waitKey(0)
         return self.cXY
@@ -104,13 +104,14 @@ class ROI():
 
     def crop(self, frame):
         self.global_heigh, self.global_width = frame.shape[:2]
-        #bounds of the roi
+        # bounds of the roi
         xmin, xmax, ymin, ymax = self.get_bounds()
         window = frame[ymin:ymax, xmin:xmax, :]
         return window
 
     def update_center(self):
         pass
+
 
 class ObjectTracker():
     """docstring for ObjectTracker"""
@@ -126,17 +127,16 @@ class ObjectTracker():
     def track(self, frame):
         # get only the ROI from the current frame
         window = self.roi.crop(frame)
-        
+
         # detect the object using the tracking method
         self.mask, centroid = self.tracking_method.detect(window)
-       
+
         # update the roi center using current ant coordinates
         self.roi.recenter(centroid)
 
         # update data
         self.history.append(self.roi.cXY)
         self.roi.prev_cXY = self.roi.cXY
-  
 
 
 class CameraTracker():
@@ -169,13 +169,13 @@ class CameraTracker():
         self.mse.append(err)
 
         return True, 'Camera Tracked'
-        
+
 
 class TrackingScenario():
     """docstring for TrackingScenario"""
     def __init__(self, object_trackers, camera_tracker=None, undistorter=None):
-        self.object_trackers = object_trackers   
-        self.camera_tracker = camera_tracker 
+        self.object_trackers = object_trackers
+        self.camera_tracker = camera_tracker
         self.iteration_counter = 0
         self.auto_mode = True
         self.undistorter = undistorter
@@ -184,11 +184,22 @@ class TrackingScenario():
     def __digest_video_path(self, video_path):
         # TODO: Validate the path
         self.video_path = video_path
-        self.cap = cv2.VideoCapture(video_path)                        # create capture object
-        self.frame_count = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT)) # total number of frames in the video file
-        self.fps = self.cap.get(cv2.CAP_PROP_FPS)                      # frames per seconds
-        self.w = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))           # frame width
-        self.h = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))          # frame height
+
+        # create capture object
+        self.cap = cv2.VideoCapture(video_path)
+
+        # total number of frames in the video file
+        self.frame_count = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        # frames per seconds
+        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
+
+        # frame width
+        self.w = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+        # frame height
+        self.h = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
         self.dim = (self.w, self.h)
 
         self.first_frame = 0
@@ -198,7 +209,6 @@ class TrackingScenario():
             frame = self.undistorter.fix(frame)
         return frame
 
-
     def show_frame(self, frame, show_frame_id=True):
         # cXY, region, features, frame_numb, mask
         frame = frame.copy()
@@ -206,38 +216,43 @@ class TrackingScenario():
         # draw region in which features are detected
         if self.camera_tracker:
             x0, xf, y0, yf = self.camera_tracker.roi.get_bounds()
-            cv2.putText(frame, 'Camera Tracking region', (x0+5, yf-5), 
-                cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.2, (0,0,255), 1, cv2.LINE_AA)
-            cv2.rectangle(frame, (x0, y0), (xf, yf), (0,0,255), 2)
+
+            cv2.putText(frame, 'Camera Tracking region', (x0+5, yf-5),
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.2, (0, 0, 255), 1,
+                        cv2.LINE_AA)
+
+            cv2.rectangle(frame, (x0, y0), (xf, yf), (0, 0, 255), 2)
             p2, p3 = self.camera_tracker.features
             # draw detected and estimated features
             for p2_, p3_ in zip(p2, p3):
                 x2, y2 = np.rint(p2_).astype(np.int32)
                 x3, y3 = np.rint(p3_).astype(np.int32)
 
-                cv2.circle(frame, (x2,y2), 3, (0,0,0), -1)
-                cv2.circle(frame, (x3,y3), 3, (0,255,0), -1)
+                cv2.circle(frame, (x2, y2), 3, (0, 0, 0), -1)
+                cv2.circle(frame, (x3, y3), 3, (0, 255, 0), -1)
 
         for otrack in self.object_trackers:
             # TODO: Do this better:
             # alter the blue channel in ant-related pixels
             window = otrack.roi.crop(frame)
-            window[:,:,0] = otrack.mask
+            window[:, :, 0] = otrack.mask
 
             # draw a point over the roi center and draw bounds
             x1, x2, y1, y2 = otrack.roi.get_bounds()
             cv2.circle(frame, otrack.roi.cXY, 5, (255, 255, 255), -1)
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0,255,255), 2)
-            cv2.putText(frame, otrack.name, (x1+5, y2-5), 
-                cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.2, (0,255,255), 1, cv2.LINE_AA)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 255), 2)
+            cv2.putText(frame, otrack.name, (x1+5, y2-5),
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.2, (0, 255, 255), 1,
+                        cv2.LINE_AA)
 
         if show_frame_id:
             h, w = frame.shape[:2]
             frame_id = self.iteration_counter + self.first_frame
             x_, y_ = .02, .05
             x, y = int(x_ * w), int(y_ * h)
-            cv2.putText(frame, str(frame_id), (x, y), 
-                cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.2, (0,255,255), 1, cv2.LINE_AA)
+            cv2.putText(frame, str(frame_id), (x, y),
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.2, (0, 255, 255), 1,
+                        cv2.LINE_AA)
 
         frame = resize_frame(frame)
         cv2.imshow('PYTANAL processing window', frame)
@@ -251,7 +266,7 @@ class TrackingScenario():
 
         # Capture the first frame to process
         ret, prev_frame = self.cap.read()
-        
+
         # correct spherical distortion
         self.prev_frame = self.__undistort__(prev_frame)
 
@@ -288,11 +303,12 @@ class TrackingScenario():
             exit()
 
     def __regular_iteration__(self):
-        # get current frame and ends the processing when no more frames are detected
+        # get current frame and ends the processing when no more frames are
+        # detected
         ret, frame = self.cap.read()
-        if not ret: 
+        if not ret:
             return False, '[INFO] All frames were processed.'
-        
+
         # correct spherical distortion
         frame = self.__undistort__(frame)
 
@@ -304,21 +320,18 @@ class TrackingScenario():
             roi_array.append(otrack.roi.get_bounds())
             otrack.track(frame)
             roi_array.append(otrack.roi.get_bounds())
- 
-        
+
         ret, message = self.camera_tracker.track(self.prev_frame, frame, roi_array)
         frame_id = self.iteration_counter + self.first_frame
 
         if not ret:
             return False, '[Error] {} (Frame {})'.format(message, frame_id)
-       
 
         # display the full image with the ant in blue (TODO: Refactor this to make more general)
         self.show_frame(frame)
 
         # save current frame and ROI center as previous for next iteration
         self.prev_frame = frame.copy()
-
 
         # Call the keyboard controller to handle key interruptions
         self.keyboard_controller()
@@ -332,10 +345,10 @@ class TrackingScenario():
         self.cap.release()
         cv2.destroyAllWindows()
 
-
     def __export_data__(self):
-        # TODO: This function needs to be rewritten to be able to handle more than 1 object tracker
-        # and also to provide more general purpe semantic information
+        # TODO: This function needs to be rewritten to be able to handle more
+        # than 1 object tracker and also to provide more general purpe semantic
+        # information
         last_frame = self.first_frame + self.iteration_counter
         percent_video = 100 * (self.first_frame + self.iteration_counter) / self.frame_count
         minutes_video = last_frame / self.fps / 60
@@ -345,12 +358,11 @@ class TrackingScenario():
             'first_frame': self.first_frame,
             'last_frame': last_frame,
             'percent': percent_video,
-            'r_ac' : self.object_trackers[0].history,
+            'r_ac': self.object_trackers[0].history,
             'affine_params': self.camera_tracker.history,
             'mse': self.camera_tracker.mse
         }
         self.__save_data__(data, minutes_video, percent_video)
-
 
     def __save_data__(self, data, minutes=None, percent=None):
         if not (minutes or percent):
@@ -368,7 +380,7 @@ class TrackingScenario():
 
     def track(self, video_path, start_in_frame=0):
         self.__digest_video_path(video_path)
-        
+
         if self.iteration_counter == 0:
             retval, message = self.__first_iteration__(start_in_frame)
             if not retval:
@@ -385,4 +397,3 @@ class TrackingScenario():
         self.__release_cap__()
         self.__export_data__()
         return retval, message
-
