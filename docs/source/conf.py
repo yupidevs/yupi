74579e0ffbbc72e43a34d5d94f8919c2f68d3893
Jorge Morgado <jorge.morgadov@gmail.com>
2021-03-06 18:38:49 -0500

    set version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2021, Gustavo Viera, Alfredo Reyes, Jorge Morgado, Ernesto Altshule
 author = 'Gustavo Viera, Alfredo Reyes, Jorge Morgado, Ernesto Altshuler'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.1'
 
 
 # -- General configuration ---------------------------------------------------
