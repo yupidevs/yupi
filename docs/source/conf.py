ad1d6987c11db2cf8658daa0daf83f6abd0afcbc
Gustavo Viera López <gvieralopez@gmail.com>
2021-03-08 11:10:08 -0500

    Update project version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2021, Gustavo Viera-López, Alfredo Reyes, Jorge Morgado, Ernesto A
 author = 'Gustavo Viera-López, Alfredo Reyes, Jorge Morgado, Ernesto Altshuler'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.1'
+release = '0.2.2'
 
 
 # -- General configuration ---------------------------------------------------
--- yupi/__init__.py
+++ yupi/__init__.py
@@ -15,4 +15,4 @@ __all__ = [
     'Trajectory'
 ]
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
