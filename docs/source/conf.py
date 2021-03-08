1764363ddb23954601e6ca0a77f31acff1349758
Jorge Morgado <jorge.morgadov@gmail.com>
2021-03-08 12:28:31 -0500

    fix type hints on docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,6 +40,7 @@ extensions = [
     # 'numpydoc',
     'sphinx_rtd_theme'
 ]
+autodoc_typehints = 'description'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
