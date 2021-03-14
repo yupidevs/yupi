7b10755352cef7e7583bca199d390454fb8711dd
alpelito7 <alpelito7@gmail.com>
2021-03-14 07:44:53 -0700

    method: displacement to the n-th order

--- yupi/trajectory.py
+++ yupi/trajectory.py
@@ -336,3 +336,21 @@ class Trajectory():
 
         theta = self.wrap(theta, degrees, wrapper)
         return theta
+
+
+    # get displacements to the n-th order
+    def get_dr_n(self, time_avg=True, lag=None, order=2):
+        # ensemble average
+        if not time_avg:
+            dx_n = (self.x - self.x[0])**order
+            dy_n = (self.y - self.y[0])**order
+            dr_n = (dx_n + dy_n)**(order / 2)
+        # time average
+        else:
+            dr_n = np.empty(lag)
+            for lag_ in range(1, lag + 1):
+                dx_n = (self.x[lag_:] - self.x[:-lag_])**order
+                dy_n = (self.y[lag_:] - self.y[:-lag_])**order
+                dr_n[lag_ - 1] = np.mean(dx_n + dy_n)
+        
+        return dr_n
