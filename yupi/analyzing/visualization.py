9a9a84a57f3da69b85e8549e0570d99c86150814
Gustavo Viera López <gvieralopez@gmail.com>
2021-03-23 16:49:56 +0100

    Update plot_trajectory parameters

--- yupi/analyzing/visualization.py
+++ yupi/analyzing/visualization.py
@@ -1,11 +1,17 @@
 import matplotlib.pyplot as plt
 
-def plot_trajectories(trajectories):
+def plot_trajectories(trajectories, max_trajectories=None, title="", legend=True, plot=True):
     # TODO: Check if trajectories is list of Trajectory
     # or Trajectory, if the second case traj = [traj]
     # If none of both case raise exception
-    for t in trajectories:
 
+    if max_trajectories is None:
+        max_trajectories = len(trajectories)
+
+
+    for i, t in enumerate(trajectories):
+        if i == max_trajectories:
+            break
         # plotting
         traj_plot = plt.plot(t.x, t.y, '-')
         color = traj_plot[-1].get_color()
@@ -16,13 +22,15 @@ def plot_trajectories(trajectories):
         plt.plot(t.x[-1], t.y[-1], 'o', alpha=.5,
                  label=f'{t.id} final position', color=color)
 
-        plt.legend(fontsize=12)
+        if legend:
+            plt.legend(fontsize=12)
 
-        plt.title('Trajectories in global coordinates')
+        plt.title(title)
         plt.tick_params(direction='in', labelsize=12)
         plt.axis('equal')
         plt.grid(True)
         plt.xlabel('x [m]', fontsize=12)
         plt.ylabel('y [m]', fontsize=12)
-
-    plt.show()
+        
+    if plot:
+        plt.show()
