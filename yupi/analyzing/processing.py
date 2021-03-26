b9854ae90464aca88f486b5732f5aa20d54c8020
alpelito7 <alpelito7@gmail.com>
2021-03-26 08:45:50 -0700

    Add docstring for turning angles

--- yupi/analyzing/processing.py
+++ yupi/analyzing/processing.py
@@ -2,23 +2,29 @@ import numpy as np
 from yupi.analyzing.transformations import wrap_theta
 
 def turning_angles(traj, accumulate=False, degrees=False, centered=False):
-    """Docstring for turning angles
+    """Return the sequence of turning angles that form the trajectory.
 
     Parameters
     ----------
     traj : Trajectory
-        [description]
+        Trajectory object.
     accumulate : bool, optional
-        [description], by default False
+        If True, turning angles are measured with respect to an axis define by the \\
+        initial velocity (i.e., angles between initial and current velocity). \\
+        Otherwise, relative turning angles are computed (i.e., angles between \\
+        succesive velocity vectors). By default False.
     degrees : bool, optional
-        [description], by default False
+        If True, angles are given in degrees. Otherwise, the units are radians. \\
+        By default False.
     centered : bool, optional
-        [description], by default False
+        If True, angles are wrapped on the interval ``[-pi, pi]``. Otherwise, \\
+        the interval ``[0, 2*pi]`` is chosen. By default False.
 
     Returns
     -------
-    [type]
-        [description]
+    np.ndarray
+        Turning angles where each position in the array correspond to a given \\
+        time instant.
     """
 
     dx = traj.x_diff()
