40d95cb92db5086d1e76c28e1794e2d3297bf206
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2021-08-05 18:23:41 -0400

    Add some checkers decorators on statistics

--- yupi/analyzing/statistics.py
+++ yupi/analyzing/statistics.py
@@ -30,6 +30,15 @@ def _check_same_dim(func):
         return func(trajs, *args, **kwargs)
     return wrapper
 
+def _check_exact_dim(dim):
+    def _check_exact_dim_decorator(func):
+        def wrapper(trajs: List[Trajectory], *args, dim=dim, **kwargs):
+            if any((t.dim != dim for t in trajs)):
+                raise ValueError(f"All trajectories must be {dim}-dimensional")
+            return func(trajs, *args, **kwargs)
+        return wrapper
+    return _check_exact_dim_decorator
+
 def _check_same_r0(func):
     def wrapper(trajs: List[Trajectory], *args, **kwargs):
         if trajs:
@@ -39,7 +48,26 @@ def _check_same_r0(func):
         return func(trajs, *args, **kwargs)
     return wrapper
 
+def _check_same_lenght(func):
+    def wrapper(trajs: List[Trajectory], *args, **kwargs):
+        if trajs:
+            length = len(trajs)
+            if any((abs(len(t) - length) > _threshold for t in trajs)):
+                raise ValueError("All trajectories must have the same length")
+        return func(trajs, *args, **kwargs)
+    return wrapper
+
+def _check_same_t(func):
+    def wrapper(trajs: List[Trajectory], *args, **kwargs):
+        if trajs:
+            length = len(trajs)
+            if any((abs(len(t) - length) > _threshold for t in trajs)):
+                raise ValueError("All trajectories must have the same length")
+        return func(trajs, *args, **kwargs)
+    return wrapper
+
 @_check_same_dt
+@_check_exact_dim(2)
 @_check_uniform_time_spaced
 def estimate_turning_angles(trajs: List[Trajectory], accumulate=False,
                             degrees=False, centered=False, wrap=True):
@@ -75,6 +103,7 @@ def estimate_turning_angles(trajs: List[Trajectory], accumulate=False,
     return np.concatenate(theta)
 
 
+@_check_same_dim
 def estimate_velocity_samples(trajs: List[Trajectory], step: int = 1):
     """
     Estimate speeds of the list of trajectories, ``trajs``,
@@ -98,6 +127,8 @@ def estimate_velocity_samples(trajs: List[Trajectory], step: int = 1):
     return np.concatenate([traj.v.norm for traj in trajs_])
 
 
+@_check_same_t
+@_check_same_lenght
 def estimate_msd_ensemble(trajs: List[Trajectory]):
     """
     Compute the square displacements for every Trajectory object
@@ -134,6 +165,8 @@ def estimate_msd_ensemble(trajs: List[Trajectory]):
     return msd
 
 
+@_check_same_dt
+@_check_uniform_time_spaced
 def estimate_msd_time(trajs: List[Trajectory], lag: int):
     """
     Estimate the mean square displacement for every Trajectory
@@ -181,6 +214,7 @@ def estimate_msd_time(trajs: List[Trajectory], lag: int):
     return msd
 
 
+@_check_same_dim
 def estimate_msd(trajs: List[Trajectory], time_avg=True, lag=None):
     """
     Estimate the mean square displacement of the list of Trajectory
@@ -220,6 +254,7 @@ def estimate_msd(trajs: List[Trajectory], time_avg=True, lag=None):
     return msd_mean, msd_std
 
 
+@_check_same_t
 def estimate_vacf_ensemble(trajs: List[Trajectory]):
     """
     Compute the pair-wise dot product between initial and current
@@ -254,6 +289,8 @@ def estimate_vacf_ensemble(trajs: List[Trajectory]):
     return vacf
 
 
+@_check_same_dt
+@_check_uniform_time_spaced
 def estimate_vacf_time(trajs: List[Trajectory], lag: int):
     """
     Estimate the velocity autocorrelation function for every
@@ -304,6 +341,7 @@ def estimate_vacf_time(trajs: List[Trajectory], lag: int):
     return vacf
 
 
+@_check_same_dim
 def estimate_vacf(trajs: List[Trajectory], time_avg=True, lag: int = None):
     """
     Estimate the velocity autocorrelation function of the list of
@@ -343,7 +381,6 @@ def estimate_vacf(trajs: List[Trajectory], time_avg=True, lag: int = None):
     return vacf_mean, vacf_std
 
 
-# kurtosis (ensemble average)
 # TODO: Fix this implementation for dim != 2 Traj
 def estimate_kurtosis_ensemble(trajs: List[Trajectory]):
     """[summary]
