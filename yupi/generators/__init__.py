af3b053f9aecd593eb4de3f6ea8ef65d182670fe
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2021-08-07 22:32:09 -0400

    Add simple docstring on inits files

--- yupi/generators/__init__.py
+++ yupi/generators/__init__.py
@@ -1,5 +1,5 @@
 """
-This submodule contains different statistical models to
+This module contains different statistical models to
 generate trajectories given certain statistical constrains.
 
 All the resources of this module should be imported directly
--- yupi/tracking/__init__.py
+++ yupi/tracking/__init__.py
@@ -1,12 +1,11 @@
 """
-This submodule is designed to capture Trajectory objects from
+This module is designed to capture Trajectory objects from
 a sequence of images. It contains different tools to extract
 the trajectories based on the color or the motion of the object
 being tracked.
 
 All the resources of this module should be imported directly
-from ``yupi.tracking``. However, we ordered the resources
-according the functionality into the following categories.
+from ``yupi.tracking``.
 """
 
 from yupi.tracking.trackers import (
