c3885dc3c3d6007eb827cc2bf95f4abed5eb9d17
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2025-04-30 13:52:38 +0000

    Set version v1.0.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ copyright = "2021, Gustavo Viera-López, Alfredo Reyes, Jorge Morgado, Ernesto A
 author = "Gustavo Viera-López, Alfredo Reyes, Jorge Morgado, Ernesto Altshuler"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+release = "1.0.2"
 
 
 # -- General configuration ---------------------------------------------------
--- yupi/__init__.py
+++ yupi/__init__.py
@@ -41,4 +41,4 @@ __all__ = [
     "WindowType",
 ]
 
-__version__ = "1.0.0"
+__version__ = "1.0.2"
